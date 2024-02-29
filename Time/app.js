3c483de313279724751f3611fc7cc3069d8347cb
Mikael H <mikael.holsve@gmail.com>
2024-02-29 20:26:12 +0100

    Added Time assignment

---
+++ Time/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/homepage.html`);
+});
+
+app.listen(PORT, () => {console.log("server listening on port", PORT)});
